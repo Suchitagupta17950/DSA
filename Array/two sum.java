f0b31e48d81caba40bbc10193451de4b8ddc2090
Suchita Gupta <suchitagupta17950@gmail.com>
2026-06-22 23:37:50 +0530

    Added LC-1 Two Sum solution

---
+++ Array/two sum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Array{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        // code here
+        int p=x+y;
+        int q=x-y;
+        int r=x*y;
+        int s=x/y ;
+        int t=x%y;
+        
+        System.out.println(p + " " + q  +" "+ r +" " + s +" " + t);
+        
+
+        // The below code prints the output
+    
+    }
+}
