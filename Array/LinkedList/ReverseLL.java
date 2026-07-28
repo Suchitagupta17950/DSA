cd9512439a8f116f5ba24446c7908ced14a2abbe
Suchita Gupta <suchitagupta17950@gmail.com>
2026-07-28 13:08:34 +0530

    ReverseLL

---
+++ Array/LinkedList/ReverseLL.java
@@ -0,0 +1,18 @@
+package LinkedList;
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode temp=head;
+        Stack<Integer>st=new Stack<>();
+        while(temp!=null){
+            st.push(temp.val);
+            temp=temp.next;
+        }
+        temp=head;
+        while(temp!=null){
+            temp.val=st.pop();
+            temp=temp.next;
+        }
+        return head;
+    }
+}
