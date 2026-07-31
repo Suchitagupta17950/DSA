806ef9f8b78f3206d29deffdeab5ca6f20059b94
Suchita Gupta <suchitagupta17950@gmail.com>
2026-07-31 23:34:23 +0530

    Single Number III

---
+++ Bit/singlenumber3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor=0;
+        for(int num:nums){
+            xor=xor^num;
+        }
+        int mask=xor&(~xor);
+        int a=0;
+        int b=0;
+
+        for(int num:nums){
+            if((num&mask)!=0){
+                a=a^num;
+            }
+            else{
+                b=b^num;
+            }
+        }
+        return new int[]{a,b};
+    }
+
+}
