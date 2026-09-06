33eeddda0e0e043ecc5d8ca09e78e7dee3ff430e
Suchita Gupta <suchitagupta17950@gmail.com>
2026-09-06 23:39:34 +0530

    Equilibriumpoint

---
+++ Array/equlbriumpoint.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static int findEquilibrium(int arr[]) {
+        // code here
+        int totalsum=0;
+        int prefixsm=0;
+        
+        for(int i:arr) totalsum+=i;
+        for(int i=0;i<arr.length;i++){
+            int suffixsm=totalsum-prefixsm-arr[i];
+            
+            if(suffixsm==prefixsm) return i;
+            
+            prefixsm=prefixsm+arr[i];
+        }
+        
+        return -1;
+        
+    }
+}
