357e61d4512e6b1cd9f3871c148325d9865545b0
Suchita Gupta <suchitagupta17950@gmail.com>
2026-09-06 23:49:25 +0530

    MISSING NUMBER WITH TWO APPROACHES

--- Array/missing number.java
+++ Array/missing number.java
@@ -12,3 +12,19 @@ class Solution {
         return (int)(expectedSum-actualSum);
     }
 }
+// Two approaches to find the missing number in an array of size n containing numbers from 1 to n+1. The first approach uses the sum formula, while the second approach uses XOR operation to find the missing number.
+class Solution {
+    int missingNum(int arr[]) {
+        // code here
+        int n=arr.length+1;
+        int xor1=0;
+        for(int i=1;i<=n;i++){
+            xor1=xor1^i;
+        }
+        int xor2=0;
+        for(int num:arr){
+            xor2^=num;
+        }
+        return xor1^xor2;
+    }
+}
